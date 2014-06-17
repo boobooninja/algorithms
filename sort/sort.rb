a1e53d3171112e2e18bf0052a665dc7acf0a406b
Loren Hale <loren.hale@gmail.com>
2014-06-17 15:04:37 -0500

    start sort

---
+++ sort/sort.rb
@@ -0,0 +1,19 @@
+module Sort
+
+  def self.selection_sort(array)
+    length = array.length
+
+    array.each_with_index do |num,i|
+      pointer = 1
+
+      while pointer < length do
+        if num > array[pointer]
+          array[i], array[pointer] = array[pointer], array[i]
+        end
+        pointer += 1
+      end
+    end
+
+    array
+  end
+end
---
+++ sort/spec/sort_spec.rb
@@ -0,0 +1,34 @@
+require 'rubygems'
+require 'rspec'
+require_relative '../sort.rb'
+
+describe 'Sort' do
+  describe '.selection_sort' do
+
+    context 'when the argument is empty' do
+      it "returns an empty array" do
+        expect(Sort.selection_sort([])).to eq([])
+      end
+    end
+
+    context 'when the argument has negative numbers' do
+      it "sorts them appropriately" do
+        expect(Sort.selection_sort([4,1,-2])).to eq([-2,1,4])
+      end
+    end
+
+    context "when the argument contains duplicate numbers" do
+      it "sorts the first before the last" do
+        expect(Sort.selection_sort([4,2,7,2])).to eq([2,2,4,7])
+      end
+    end
+
+    context "when the argument is a large unordered array" do
+      it "sorts them appropriately from small to large" do
+        unsorted = [9,3,6,1,3,0,6,4,5,1,22,45,12,33,99,56,73]
+        sorted   = [0, 1, 1, 3, 3, 4, 5, 6, 6, 9, 12, 22, 33, 45, 56, 73, 99]
+        expect(Sort.selection_sort(unsorted)).to eq(sorted)
+      end
+    end
+  end
+end

d6939313f90b3000a04af4cf6bba8f795b09a7e2
Loren Hale <loren.hale@gmail.com>
2014-06-17 14:15:46 -0500

    add queue

---
+++ queue/queue.rb
@@ -0,0 +1,51 @@
+class Node < Array
+  def initialize(value)
+    this = super()
+    this << value
+    this << nil
+  end
+
+  def value
+    self[0]
+  end
+
+  def pointer
+    self[1]
+  end
+
+  def pointer=(value)
+    self[1] = value
+  end
+end
+
+class Queue
+  def initialize
+    @nodes = { }
+    @first = nil
+    @last  = nil
+  end
+
+  def push(node)
+    if (@last)
+      @nodes[ @last ].pointer = node.value
+      @nodes[ node.value ] = node
+      @last = node.value
+    else
+      @nodes[ node.value ] = node
+      @first = node.value
+      @last = node.value
+    end
+  end
+
+  def shift
+    this_node = @nodes[ @first ]
+    @nodes.delete(@first)
+
+    @first = @nodes[ this_node.pointer ].value
+    this_node
+  end
+
+  def peek
+    @nodes[ @first ]
+  end
+end

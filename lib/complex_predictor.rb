b341e5095757d29d767b7435fc04d4bc53ae3ad1
Loren Hale <loren.hale@gmail.com>
2014-06-24 16:02:33 -0500

    96 percent accuracy

--- lib/complex_predictor.rb
+++ lib/complex_predictor.rb
@@ -9,14 +9,6 @@ class ComplexPredictor < Predictor
   def train!
     @data = {}
 
-    # {
-    #   philosophy: {
-    #     words: {'angst' => 4, 'christianity' => 7},
-    #     word_count: 1000,
-    #     books: 10,
-    #   }
-    # }
-
     @all_books.each do |category, books|
       @data[category] ||= {
                             words: Hash.new(0.0),
@@ -34,12 +26,6 @@ class ComplexPredictor < Predictor
         @data[category][:books] += 1
       end
     end
-
-    # max_word = nil
-    # max_count = 0
-    # @data.each do |category, category_hash|
-
-    # end
   end
 
   # Public: Predicts category.
@@ -57,15 +43,19 @@ class ComplexPredictor < Predictor
 
     tokens.each do |token|
       if good_token?(token)
+
         highest = nil
         highest_count = 0.0
 
         @data.each do |category, category_hash|
-          # if category_hash[:words][token]
-            word_count = category_hash[:words][token] / category_hash[:books]
-            highest = category if word_count > highest_count
-            highest_count = category_hash[:words][token]
-          # end
+          if category_hash[:words][token]
+            word_count = category_hash[:words][token] / (category_hash[:word_count] / category_hash[:books])
+
+            if word_count > highest_count
+              highest = category
+              highest_count = word_count
+            end
+          end
         end
 
         if highest
@@ -76,41 +66,15 @@ class ComplexPredictor < Predictor
 
     highest = nil
     highest_count = 0
-    good_token_count = 0
+
     this_books_categories.each do |category, count|
-puts "#{category} : #{count} : #{count / @data[category][:books]}"
       if count > highest_count
         highest = category
         highest_count = count
       end
-      good_token_count += count
     end
-puts "highest: #{highest}"
+
     highest
-
-
-    minimum_category = nil
-    minimum_distance = 999999999999
-
-    @data.each do |category, counts|
-      average_words_per_book = counts[:word_count] / counts[:books]
-      difference = (good_token_count - average_words_per_book).abs
-
-      if difference < minimum_distance
-        minimum_category = category
-        minimum_distance = difference
-      end
-    end
-
-    minimum_category
-puts "minimum_category : #{minimum_category}"
-
-    if this_books_categories[highest] < this_books_categories[minimum_category]
-      highest
-    else
-      minimum_category
-    end
-
   end
 end
 
